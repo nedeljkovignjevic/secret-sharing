173a0a70bf95fe043fdc57b8a5515a9e43c8d5e7
Nedeljko Vignjevic <vignjevic.nedeljko1999@gmail.com>
2019-12-01 23:21:57 +0100

    Code reformat

--- src/main.py
+++ src/main.py
@@ -1,9 +1,32 @@
 import matplotlib.image as mpimg
 from PIL import Image
 import numpy as np
-from scheme import Scheme
+
+from tkinter import simpledialog as sd
+
+from pic import split_parts_list, browse_img, datetime, reconstruct_image
+
 
 if __name__ == '__main__':
+
+    # ------------------------------------------------------------
+    path = browse_img()
+    t1 = datetime.now()
+    n = int(input("Number of shares: "))
+    k = int(input("Number of needed shares for reconstruction: "))
+    # split_parts(5, 3, 255, load_img(path), path)
+    split_parts_list(n, k, 257, mpimg.imread(path), path)
+    print((datetime.now() - t1).seconds)
+    # unos = sd.askinteger("Number of shares for reconstruction of secret?",
+    #                     "Input in range (" + str(k) + "-" + str(n) + ")",
+    #                     initialvalue=k, minvalue=k, maxvalue=n)
+    # imgs = []
+    # for i in range(unos):
+    #    imgs.append(browse_img())
+    # reconstruct_image(imgs, n, k, unos)
+    # ------------------------------------------------------------
+
+    # ------------------------------------------------------------
     # secret = int(input("Unesite broj kao sifru: "))
     # s = Scheme(secret, 5, 3, 104729)
     # shares = s.construct_shares()
@@ -17,22 +40,19 @@ if __name__ == '__main__':
     #
     # returned_secret = s.reconstruct_secret(shares, inputs, 5, 3)
     # print(returned_secret)
+    # ------------------------------------------------------------
 
-    #-------------------------------------
-    #Convert image to numpy array
-    img = mpimg.imread('../res/imag.jpg')
-    print(img)
-
-    # # Summarize shape
-    # # print(img.shape)
+    # ------------------------------------------------------------
+    # Summarize shape
+    # print(img.shape)
     # a, b, c = img.shape
     # print(a, b, c)
     #
-    # # Create Pillow image
+    # Create Pillow image
     # image2 = Image.fromarray(img)
     #
-    # # lists = (np.empty(shape=(1, b)) for i in range(3))
+    # lists = (np.empty(shape=(1, b)) for i in range(3))
     # new_rows = [np.empty(shape=(1, 3)) for i in range(3)]
     # print(list(new_rows))
     # print(list(new_rows)[0][1])
-    #-------------------------------------
+    # ------------------------------------------------------------
--- src/pic.py
+++ src/pic.py
@@ -1,33 +1,76 @@
 from PIL import Image
+import matplotlib.image as mpimg
+
+from tkinter import filedialog as fd
+from datetime import datetime
 import random
 import functools
-import matplotlib.image as mpimg
-from scheme import Scheme, np
 import os
-from datetime import datetime
-import sys
 
-if sys.version_info[0] < 3:
-    import Tkinter as tk
-    import tk.tkFileDialog as fd
-    import tk.tkSimpleDialog as sd
-else:
-    import tkinter as tk
-    from tkinter import filedialog as fd
-    from tkinter import simpledialog as sd
+from scheme import Scheme, np
 
-_PRIME = 2**127 - 1
+_PRIME = 2 ** 127 - 1
 _RINT = functools.partial(random.SystemRandom().randint, 0)
 
 
 def browse_img():
     while True:
-        pname = fd.askopenfilename(filetypes=[("Image format", ".tif .tiff .jpg .jpeg .gif .png .bmp .eps .raw .cr2 .nef .orf .sr2")])
+        pname = fd.askopenfilename(
+            filetypes=[("Image format", ".tif .tiff .jpg .jpeg .gif .png .bmp .eps .raw .cr2 .nef .orf .sr2")])
         print(pname)
         if pname is not None:
             break
     return pname
 
+
+def split_parts_list(n, k, prime, img, path):
+    h, w, rgb = img.shape
+    name, ext = os.path.splitext(path)
+    np_lists = [[] for i in range(n)]
+    t1 = datetime.now()
+    for row in img:
+        new_rows = [[] for j in range(n)]
+        r, g, b = row[0][0], row[0][1], row[0][2]
+        if len(row[0]) > 3:
+            r, g, b = row[0][1], row[0][2], row[0][3]
+        for pix in row:
+            p1, p2, p3 = Scheme(r, n, k, prime), Scheme(g, n, k, prime), Scheme(b, n, k, prime)
+            sh1, sh2, sh3 = p1.construct_shares(), p2.construct_shares(), p3.construct_shares()
+            for i in range(n):
+                new_rows[i].append([sh1[i + 1], sh2[i + 1], sh3[i + 1]])
+
+        v = 0
+        for el in range(n):
+            np_lists[el].append(new_rows[el])
+            v += 1
+    i = 0
+    for image in np_lists:
+        new_img = Image.fromarray(np.array(image), 'RGB')
+        new_img.save(name + "_share" + str(i) + ext)
+        i += 1
+
+
+def reconstruct_image(images, n, k, unos):
+    imgs = [mpimg.imread(i) for i in images]
+    row_count = 0
+    for row in imgs[0]:
+        rec_pix = [[] for i in range(n)]
+        pix_count = 0
+        for pix in row:
+            shares_r = {}
+            shares_g = {}
+            shares_b = {}
+            for j in range(unos):
+                shares_r[j] = imgs[j][row_count][pix_count][0]
+                shares_g[j] = imgs[j][row_count][pix_count][1]
+                shares_b[j] = imgs[j][row_count][pix_count][2]
+            [r, g, b] = [rec_secret(shares_r)]
+
+
+def rec_secret(shares):
+    pass
+
+# -------------------------------------------------------------------------------------------------------------
 # sa numpy array
 # def split_parts(n, k, prime, img, path):
 #     h, w, rgb = img.shape
@@ -56,69 +99,4 @@ def browse_img():
 #         new_img = Image.fromarray(image, 'RGB')
 #         new_img.save(name + "_share" + str(i) + ext)
 #         i += 1
-
-def split_parts_list(n, k, prime, img, path):
-    h, w, rgb = img.shape
-    name, ext = os.path.splitext(path)
-    np_lists = [[] for i in range(n)]
-    t1 = datetime.now()
-    for row in img:
-        new_rows = [[] for j in range(n)]
-        r, g, b = row[0][0], row[0][1], row[0][2]
-        if len(row[0]) > 3:
-            r, g, b = row[0][1], row[0][2], row[0][3]
-        for pix in row:
-            p1, p2, p3 = Scheme(r, n, k, prime), Scheme(g, n, k, prime), Scheme(b, n, k, prime)
-            sh1, sh2, sh3 = p1.construct_shares(), p2.construct_shares(), p3.construct_shares()
-            for i in range(n):
-                new_rows[i].append([sh1[i+1], sh2[i+1], sh3[i+1]])
-
-        v = 0
-        for el in range(n):
-            np_lists[el].append(new_rows[el])
-            v += 1
-    print(datetime.now()-t1)
-    i = 0
-    for image in np_lists:
-        new_img = Image.fromarray(np.array(image), 'RGB')
-        new_img.save(name+"_share"+str(i)+ext)
-        i += 1
-
-def reconstruct_image(images, n, k, unos):
-    imgs = [mpimg.imread(i) for i in images]
-    row_count = 0
-    for row in imgs[0]:
-        rec_pix = [[] for i in range(n)]
-        pix_count = 0
-        for pix in row:
-            shares_r = {}
-            shares_g = {}
-            shares_b = {}
-            for j in range(unos):
-                shares_r[j] = imgs[j][row_count][pix_count][0]
-                shares_g[j] = imgs[j][row_count][pix_count][1]
-                shares_b[j] = imgs[j][row_count][pix_count][2]
-            [r, g, b] = [rec_secret(shares_r)]
-
-
-def rec_secret(shares):
-    pass
-
-
-if __name__ == '__main__':
-    # poly = [_RINT(_PRIME) for i in range(3)]
-    # print(poly)
-    path = browse_img()
-    t1 = datetime.now()
-    n = int(input("Number of shares: "))
-    k = int(input("Number of needed shares for reconstruction: "))
-    #split_parts(5, 3, 255, load_img(path), path)
-    split_parts_list(n, k, 257, mpimg.imread(path), path)
-    print(datetime.now()-t1)
-    unos = sd.askinteger("Number of shares for reconstruction of secret?","Input in range ("+str(k)+"-"+str(n)+")",
-                  initialvalue=k, minvalue=k, maxvalue=n)
-    imgs = []
-    for i in range(unos):
-        imgs.append(browse_img())
-    reconstruct_image(imgs, n, k, unos)
-
+# -------------------------------------------------------------------------------------------------------------
