a66d0f8605385e48583dfb2684bdde6e25faa82f
Nedeljko Vignjevic <vignjevic.nedeljko1999@gmail.com>
2020-01-21 00:43:26 +0100

    Merge branch 'master' of C:\Users\Neobyte\Desktop\git repositories\secret-sharing with conflicts.

--- src/scheme.py
+++ src/scheme.py
@@ -2,20 +2,10 @@ from random import randint
 from lagrange_polynomial import LagrangePolynomial, np
 
 
-def printToFile(fileWrite, data):
-    with open(fileWrite, 'w') as outfile:
-        outfile.write('# Array shape: {0}\n'.format(data.shape))
-
-        for data_slice in data:
-            np.savetxt(outfile, data_slice, fmt='%-10.2f')
-            outfile.write('# New slice\n')
-
-
 class Scheme(object):
     """
     Implementation of Shamir's Secret Sharing scheme,
     """
-
     def __init__(self, s, n, k, p):
         """
         s: secret
@@ -34,25 +24,11 @@ class Scheme(object):
         self.coefs.append(self.s)
         values = np.polyval(self.coefs, [i for i in range(1, self.n + 1)]) % self.p
         shares = {i: values[i - 1] for i in range(1, self.n + 1)}
+        print(shares)
         return shares
 
-    def construct_shares_image(self):
-        self.coefs.append(self.s)
-        values = np.polyval(self.coefs, [i for i in range(1, self.n + 1)]) % self.p
-        shares = {}
-        for i in range(1, self.n + 1):
-            if int(values[i - 1]) != 256:
-                if int(values[i - 1]) == 0:
-                    shares[self.n + 1] = 256
-                shares[i] = int(values[i - 1])
-            else:
-                shares[i] = 0
-                shares[self.n + 1] = 256
-        return shares
-
-    @staticmethod
-    def reconstruct_secret(shares: dict, inputs: list, k, p):
-        if len(shares) < k:
+    def reconstruct_secret(self, shares: dict, inputs: list):
+        if len(shares) < self.k:
             raise Exception("More shares needed")
 
         for el in inputs:
@@ -70,30 +46,6 @@ class Scheme(object):
         # plt.scatter(indeksi, [shares[ind] for ind in indeksi], c='k')
         # plt.plot(indeksi, lp.interpolate(indeksi) % self.p, linestyle=':')
 
-        secret = lp.interpolate(0) % p
+        secret = lp.interpolate(0) % self.p
         # plt.show()
-        return secret
-
-    @staticmethod
-    def reconstruct_secret_img(shares: dict, inputs: list, k, p):
-        if len(shares) < k:
-            raise Exception("More shares needed")
-
-        for el in inputs:
-            if el not in shares.values():
-                raise Exception("Inadequate share")
-
-        indeksi = []
-        vals = np.array(list(shares.values()))
-        for i in range(len(inputs)):
-            search = inputs[i]
-            if inputs[i] == 256:
-                search = 0
-            ii = np.where(vals == search)[0]
-            lii = [list(shares.keys())[i] for i in ii]
-            indeksi.append(lii)
-
-        lp = LagrangePolynomial(indeksi, [shares[ind[0]] for ind in indeksi])
-
-        secret = lp.interpolate_img(0) % p
         return secret
