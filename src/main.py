24da1a95353dca82df69b5952557d6321a25c549
Nedeljko Vignjevic <vignjevic.nedeljko1999@gmail.com>
2020-01-25 01:04:48 +0100

    Added text encryption (needs some fixes).

--- src/main.py
+++ src/main.py
@@ -1,28 +1,71 @@
-from pic import browse_img, datetime, Image, np, split_parts_list, reconstruct_image
+from image_processing import browse_img, datetime, Image, np, split_parts_list, reconstruct_image
 from os import path
 from tkinter import Tk
 from tkinter import simpledialog as sd
+from scheme import Scheme
 
 if __name__ == '__main__':
 
-    path_img = browse_img()
-    name, ext = path.splitext(path_img)
-    t1 = datetime.now()
-    TK = Tk()
-    TK.withdraw()
-    n = int(input("Number of shares  >>"))
-    k = int(input("Number of needed shares for reconstruction (TWO IS MIN)   >>"))
-    pic = Image.open(path_img)
-    matrix = np.array(pic, np.int32)
-    sharesRGB = split_parts_list(n, k, 257, matrix, path_img)
-    print((datetime.now() - t1).seconds)
-    unos = sd.askinteger("Number of shares for reconstruction of secret?",
-                         "Input in range (" + str(k) + "-" + str(n) + ")",
-                         initialvalue=k, minvalue=k, maxvalue=n)
-    imgs = []
-    for i in range(unos):
-        imgs.append(browse_img())
+    # Text encryption
+    # TODO: popraviti \x - LicmMa metodom: kod construct ako je ispod 32 dodaj 32 i sacuvaj share
+    # TODO:  kod reconstruct za  32 < x < 63 naci originalno konstruisani znak
+    # ------------------------------------------------------------------
+    k = 2
+    secret = input("Unesite sifru: ")
+    text = [ord(c) for c in secret]
 
-    matrix = reconstruct_image(imgs, k, 257, sharesRGB)
-    new_img = Image.fromarray(matrix.astype('uint8'), 'RGB')
-    new_img.save("SECRET_RESTORED" + ext)
+    shares, lista = [], []
+    for el in text:
+        s = Scheme(el, 4, k, 127)
+        shares = s.construct_shares()
+        lista.append(shares)
+
+    print(lista)
+    for dic in lista:
+        dict_variable = {key: chr(value) for (key, value) in dic.items()}
+        print(dict_variable)
+
+    glavna_lista = []
+    while True:
+        word = input("Input word: (x for exit): ")
+        if word == "x": break
+        rec = [int(ord(el)) for el in word]
+        glavna_lista.append(rec)
+
+    print(glavna_lista)
+    reconstructed_secret = ""
+    b = 0
+    for dic in lista:
+        reconstructed_secret += chr(int(Scheme.reconstruct_secret(dic, glavna_lista[b], k, 127)))
+        b += 1
+
+    print(f'**** RECONSTRUTED **** \n{reconstructed_secret}\n')
+    # ------------------------------------------------------------------
+
+    # Image encryption
+    # ------------------------------------------------------------------
+    # path_img = browse_img()
+    # _, ext = path.splitext(path_img)
+    #
+    # t1 = datetime.now()
+    # TK = Tk()
+    # TK.withdraw()
+    # n = int(input("Number of shares  >>"))
+    # k = int(input("Number of needed shares for reconstruction (TWO IS MIN)   >>"))
+    #
+    # pic = Image.open(path_img)
+    # matrix = np.array(pic, np.int32)
+    # sharesRGB = split_parts_list(n, k, 257, matrix, path_img)
+    # print((datetime.now() - t1).seconds)
+    #
+    # unos = sd.askinteger("Number of shares for reconstruction of secret?",
+    #                      "Input in range (" + str(k) + "-" + str(n) + ")",
+    #                      initialvalue=k, minvalue=k, maxvalue=n)
+    # imgs = []
+    # for i in range(unos):
+    #     imgs.append(browse_img())
+    #
+    # matrix = reconstruct_image(imgs, k, 257, sharesRGB)
+    # new_img = Image.fromarray(matrix.astype('uint8'), 'RGB')
+    # new_img.save("../res/SECRET" + ext)
+    # ------------------------------------------------------------------
--- src/scheme.py
+++ src/scheme.py
@@ -2,10 +2,20 @@ from random import randint
 from lagrange_polynomial import LagrangePolynomial, np
 
 
+def printToFile(fileWrite, data):
+    with open(fileWrite, 'w') as outfile:
+        outfile.write('# Array shape: {0}\n'.format(data.shape))
+
+        for data_slice in data:
+            np.savetxt(outfile, data_slice, fmt='%-10.2f')
+            outfile.write('# New slice\n')
+
+
 class Scheme(object):
     """
     Implementation of Shamir's Secret Sharing scheme,
     """
+
     def __init__(self, s, n, k, p):
         """
         s: secret
@@ -24,11 +34,26 @@ class Scheme(object):
         self.coefs.append(self.s)
         values = np.polyval(self.coefs, [i for i in range(1, self.n + 1)]) % self.p
         shares = {i: values[i - 1] for i in range(1, self.n + 1)}
-        print(shares)
         return shares
 
-    def reconstruct_secret(self, shares: dict, inputs: list):
-        if len(shares) < self.k:
+    def construct_shares_image(self):
+        self.coefs.append(self.s)
+        values = np.polyval(self.coefs, [i for i in range(1, self.n + 1)]) % self.p
+        shares = {}
+        for i in range(1, self.n + 1):
+            if int(values[i - 1]) != 256:
+                if int(values[i - 1]) == 0:
+                    shares[self.n + 1] = 256
+                shares[i] = int(values[i - 1])
+            else:
+                shares[i] = 0
+                shares[self.n + 1] = 256
+
+        return shares
+
+    @staticmethod
+    def reconstruct_secret(shares: dict, inputs: list, k, p):
+        if len(shares) < k:
             raise Exception("More shares needed")
 
         for el in inputs:
@@ -39,13 +64,31 @@ class Scheme(object):
         for i in range(len(inputs)):
             indeksi.append(int(list(shares.keys())[list(shares.values()).index(inputs[i])]))
 
-        # print(indeksi)
-        # print([shares[ind] for ind in indeksi])
         lp = LagrangePolynomial(indeksi, [shares[ind] for ind in indeksi])
+        secret = lp.interpolate(0) % p
 
-        # plt.scatter(indeksi, [shares[ind] for ind in indeksi], c='k')
-        # plt.plot(indeksi, lp.interpolate(indeksi) % self.p, linestyle=':')
+        return secret
 
-        secret = lp.interpolate(0) % self.p
-        # plt.show()
+    @staticmethod
+    def reconstruct_secret_img(shares: dict, inputs: list, k, p):
+        if len(shares) < k:
+            raise Exception("More shares needed")
+
+        for el in inputs:
+            if el not in shares.values():
+                raise Exception("Inadequate share")
+
+        indeksi = []
+        vals = np.array(list(shares.values()))
+        for i in range(len(inputs)):
+            search = inputs[i]
+            if inputs[i] == 256:
+                search = 0
+            ii = np.where(vals == search)[0]
+            lii = [list(shares.keys())[i] for i in ii]
+            indeksi.append(lii)
+
+        lp = LagrangePolynomial(indeksi, [shares[ind[0]] for ind in indeksi])
+
+        secret = lp.interpolate_img(0) % p
         return secret
